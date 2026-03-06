0287a3077641db907b013942ab56c589c47aad8a
lucianocarstens <lcarstens@inf.ufpr.br>
2026-03-06 20:35:34 -0300

    Adicionando exercícios iniciais

---
+++ exercio_aula1.py
@@ -0,0 +1,9 @@
+variavel1 = 10
+variavel1 = variavel1 + 2
+variavel2 = variavel1 - 1
+variavel2 = variavel1 + 10
+variavel1 + 10
+variavel2 + 1
+print(variavel1)
+
+print(variavel2)
---
+++ exercio_aula1_2.py
@@ -0,0 +1,8 @@
+var1=1
+var2=3
+var2=2.5
+var3=4
+var1="var2"
+print(var1)
+print(var2)
+print(var3)     
